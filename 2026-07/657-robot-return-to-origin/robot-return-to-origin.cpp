92510a0796c2c97033ba3660be891ee93b40c45b
Snehan Maity <snehanmaity@gmail.com>
2026-07-24 23:15:05 +0530

    Time: 3 ms (44.64%) | Memory: 10.1 MB (98.40%) - LeetSync

---
+++ 2026-07/657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0, y=0;
+        for(auto it: moves){
+            if(it == 'U')   y++;
+            else if(it == 'D')  y--;
+            else if(it == 'R')  x++;
+            else    x--;
+        }
+        return (x==0 && y==0);
+    }
+};
